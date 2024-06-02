ea67ff42c2cdc7d92be3d1ebeb9ac568d27def53
Sem <semseveryanin@gmail.com>
2024-06-02 23:58:35 +0300

    added models course, lesson and added CRUD with use generics and viewsets

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from materials.models import Course, Lesson
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'preview',)
+    search_fields = ('title',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'preview', 'course')
+    list_filter = ('course',)
+    search_fields = ('title',)
--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    """ Модель курса"""
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='course/image/', default='course/image/default_course.jpg', **NULLABLE,
+                                verbose_name='Превью')
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='lesson/image/', default='lesson/image/default_lesson.jpg', **NULLABLE,
+                                verbose_name='Превью')
+    url = models.CharField(max_length=350, **NULLABLE, verbose_name='Ссылка на видео')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс')
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
