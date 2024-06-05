03a39fbd02ba04e1db41b37dba0cbe17f8ef5565
Sem <semseveryanin@gmail.com>
2024-06-05 03:12:33 +0300

    added payments model and serializers

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from materials.models import Course, Lesson
+from django.utils.translation import gettext as _
 
 NULLABLE = {"blank": True, "null": True}
 
@@ -25,3 +27,26 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payments(models.Model):
+    class PaymentMethodChoices(models.TextChoices):
+        CASH = "Наличные", _("Наличные")
+        CARD = "Карта", _("Карта")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    payment_date = models.DateTimeField(auto_now=True, verbose_name='дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, verbose_name='оплаченный курс')
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, **NULLABLE, verbose_name='оплаченный урок')
+    payment_amount = models.PositiveIntegerField(verbose_name='сумма платежа')
+    payment_method = models.CharField(default=PaymentMethodChoices.CASH, choices=PaymentMethodChoices,
+                                      verbose_name='способ оплаты')
+
+    def __str__(self):
+        return (f"{self.user} | {self.payment_date} | {self.payment_amount} | {self.payment_method} | "
+                f"{self.paid_course if self.paid_course else self.paid_lesson}")
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+        ordering = ['-payment_date',]
